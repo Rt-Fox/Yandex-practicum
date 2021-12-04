28cecdf50372f91a4a1304122a3b80e8a5641c78
Ruslan <suleandiman2@mail.ru>
2021-12-05 01:22:26 +0300

    fix bags

--- sprint2redux/src/services/http/getOrder.js
+++ sprint2redux/src/services/http/getOrder.js
@@ -1,26 +1,23 @@
-import {setOrder} from "../reducers/orderReducer";
+import {setOrder} from "../actions/orderAction";
+import {url} from "../utils/consts";
+import {checkResponse} from "./checkResponse";
+import {deleteAllIngredients} from "../actions/ingredientsAction";
 
-let url = 'https://norma.nomoreparties.space/api/orders';
 
 export const getOrder = (data) => {
 
     return dispatch => {
-        fetch(url, {
+        fetch(`${url}/orders`, {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             method: 'POST',
             body: JSON.stringify(data)
-        }).then(response => {
-            if (response.ok) {
-                return response.json()
-            }
-            return Promise.reject(response.status);
-        })
+        }) .then(checkResponse)
             .then(response => {
                 dispatch(setOrder(response));
-
+                dispatch(deleteAllIngredients())
             })
             .catch(err => console.log(err));
 
